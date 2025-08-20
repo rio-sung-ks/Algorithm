fea85bd70c004c53fa6d010d4c8dc413112d936b
rio <67265050+rio-sung-ks@users.noreply.github.com>
2025-08-20 19:16:02 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    for (let i = 0; i < list1.length; i++) {
+        console.log("11");
+        console.log(list1);
+    }
+    // list1.value
+    
+};
